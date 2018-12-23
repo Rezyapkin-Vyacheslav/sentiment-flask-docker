335bb94803f9ad6e0abb7fcda8584fbed06379f8
Rezyapkin-Vyacheslav <snikyu@gmail.com>
2018-12-24 00:21:23 +0300

    second commit

--- hello.py
+++ hello.py
@@ -11,7 +11,6 @@ from codecs import open
 import time
 
 
-
 app = Flask(__name__)
 
 print ("Preparing classifier")
@@ -22,22 +21,25 @@ print (time.time() - start_time, "seconds")
 
 @app.route('/')
 def index():
-    return "Hello, Guy!!"
+    return "Hello, very Guy!!"
 
 app.config.update(dict(
     SECRET_KEY="powerful secretkey",
     WTF_CSRF_SECRET_KEY="a csrf secret key"
 ))
-
+MAX_LEN = 10
 @app.route("/sentiment-demo", methods=["POST", "GET"])
 def index_page(text="", prediction_message=""):
     if request.method == "POST":
         text = request.form["text"]
-        with open("ydf_demo_logs.txt", "a", "utf-8") as logfile:
-            print(text)
-            print ("<response>")
-            prediction_message = "with proba={:.2} I suppose it is a good film".format(clf.predict_proba([text])[0][1])
-            print(prediction_message)
-            print ("</response>")
+        if len(text)< MAX_LEN:
+            with open("ydf_demo_logs.txt", "a", "utf-8") as logfile:
+                print(text)
+                print ("<response>")
+                prediction_message = "with proba={:.2} I suppose it is a good film".format(clf.predict_proba([text])[0][1])
+                print(prediction_message)
+                print ("</response>")
+        else:
+            prediction_message = 'Len of text must be less than 100000'
 	
     return render_template('hello.html', text=text, prediction_message=prediction_message)
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
-docker exec -it flask-hello_flask_1 bash
+docker exec -it a26adcbc0c4e bash
 
-docker exec -it flask-hello_flask_1 python train_model.py
+docker exec -it a26adcbc0c4e python model.py
 
 
 
@@ -10,3 +10,10 @@ curl --header "Content-Type: application/json" \
   http://localhost:5000/iris_post
 
   docker kill $(docker ps -q)
+
+  echo "# sentiment-flask-docker" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Rezyapkin-Vyacheslav/sentiment-flask-docker.git
+git push -u origin master
